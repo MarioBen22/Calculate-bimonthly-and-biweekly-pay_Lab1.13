b4c5de5248b170fdef96b4000bdb5a0ce90785e7
Mario Benavides Jr <36014007+MarioBen22@users.noreply.github.com>
2018-10-08 00:39:28 -0500

    Update benavides_lab1.13.cpp
    tidying up spacing and adding notes.

--- benavides_lab1.13.cpp
+++ benavides_lab1.13.cpp
@@ -1,20 +1,27 @@
 // This program calculates the bimonthly and biweekly pay.
+// Mario Benavides
+// Lab 1.13 Completed
+
+
 #include <iostream>
 using namespace std;
+
 
 int main()
 {
 	double salary, bimonthly, biweekly;
 	salary = 39000;
 	
-	cout << "Annual gross salary: \t\t   $39000  " << endl; //Display the salary.
+	cout << "Annual gross salary: \t\t   $39000  " << endl; 		//Display the salary.
 	
-	bimonthly = salary / 24; //calculate the bi-monthly.
+	bimonthly = salary / 24; 						//calculate the bi-monthly.
 	
-	biweekly = salary / 26; //calculate the bi-weekly.
+	biweekly = salary / 26; 						//calculate the bi-weekly.
 	
-	cout << "Gross pay per bi-monthly paycheck: $"  << bimonthly << endl; //Display the bi-monthly paycheck.
+	cout << "Gross pay per bi-monthly paycheck: $"  << bimonthly << endl;	 //Display the bi-monthly paycheck.
 	
-	cout << "Gross pay per bi-weekly paycheck:  $"  << biweekly << endl; //Display the bi-weekly paycheck.
+	cout << "Gross pay per bi-weekly paycheck:  $"  << biweekly << endl;	 //Display the bi-weekly paycheck.
+	
+	
 	return 0;
 }
